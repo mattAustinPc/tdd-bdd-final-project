591a8306cfffbcced10492a32b86ec155e375691
Matt Austin <matthewaustin@utilita.co.uk>
2025-01-17 21:26:15 +0000

    tdd_01 whitespace removal

--- tests/test_models.py
+++ tests/test_models.py
@@ -159,7 +159,7 @@ class TestProductModel(unittest.TestCase):
         # See if we get back 5 products
         products = Product.all()
         self.assertEqual(len(products), 5)
-        
+
     def test_find_by_name(self):
         """It should Find a Product by Name"""
         products = ProductFactory.create_batch(5)
